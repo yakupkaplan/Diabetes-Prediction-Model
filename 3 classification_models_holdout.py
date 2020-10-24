9c446eb008e0035b917fec60a3e05a812db4e4b7
Yakup Kaplan <66479180+yakupkaplan@users.noreply.github.com>
2020-10-24 22:21:28 +0200

    Add files via upload

--- 3 classification_models_holdout.py
+++ 3 classification_models_holdout.py
@@ -3,9 +3,17 @@
 # CLASSIFICATION MODELS - HOLDOUT
 
 '''
-
+Steps to follow:
+    - Load the dsaved dataset
+    - General View
+    - Modeling
+        - Base models: LogisticRegression, GaussianNB, KNeighborsClassifier, SVC, MLPClassifier, DecisionTreeClassifier,
+                       BaggingClassifier, RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier,
+                       XGBClassifier, LGBMClassifier, CatBoostClassifier, NGBClassifier
+    - Model Evaluation
+    - Model Tuning
+    - Model Results
 '''
-
 
 # Import dependencies
 import numpy as np
@@ -46,7 +54,7 @@ pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 
 # Load the preprocessed dataset
-diabetes_preprocessed = pd.read_csv(r'C:\Users\yakup\PycharmProjects\dsmlbc\projects\diabetes_classification\diabetes_prepared_robustscaled.csv')
+diabetes_preprocessed = pd.read_csv(r'C:\Users\yakup\PycharmProjects\dsmlbc\projects\diabetes_classification\diabetes_prepared.csv')
 df = diabetes_preprocessed.copy()
 
 
@@ -76,9 +84,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random
 
 
 # Evaluate each model in turn by looking at train and test errors and scores
-def evaluate_classification_model_holdout(models, X, y):
-    # Split the dataset into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=12345)
+def evaluate_classification_model_holdout(models):
+    # # Split the dataset into train and test sets
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=123456)
 
     # Define lists to track names and results for models
     names = []
@@ -103,8 +111,7 @@ def evaluate_classification_model_holdout(models, X, y):
     print('\n################ Train and test results for the model: ################\n')
     data_result = pd.DataFrame({'models': names,
                                 'accuracy_train': train_accuracy_results,
-                                'accuracy_test': test_accuracy_results
-                                })
+                                'accuracy_test': test_accuracy_results})
     print(data_result)
 
     # Plot the results
@@ -118,9 +125,9 @@ def evaluate_classification_model_holdout(models, X, y):
 
 # Define a function to plot feature_importances
 def plot_feature_importances(tuned_model):
-    Importance = pd.DataFrame({'Importance': tuned_model.feature_importances_ * 100, 'Feature': X_train.columns})
+    feature_importances = pd.DataFrame({'Importance': tuned_model.feature_importances_ * 100, 'Feature': X_train.columns})
     plt.figure()
-    sns.barplot(x="Importance", y="Feature", data=Importance.sort_values(by="Importance", ascending=False))
+    sns.barplot(x="Importance", y="Feature", data=feature_importances.sort_values(by="Importance", ascending=False))
     plt.title('Feature Importance - ') # TODO tuned_model.__name__
     plt.show()
 
@@ -132,9 +139,9 @@ def plot_confusion_matrix(model, X_test, y_test, normalize=True):
 
 
 # Function to plot confusion_matrix
-def plot_confusion_matrix_yb(model, X, y):
-    # Split the dataset into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=12345)
+def plot_confusion_matrix_yb(model):
+    # # Split the dataset into train and test sets
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=123456)
     model_cm = ConfusionMatrix(model, percent=True, classes=['not_diabets', 'has_diabetes'], cmap='Blues')
     model_cm.fit(X_train, y_train)
     model_cm.score(X_test, y_test)
@@ -142,9 +149,9 @@ def plot_confusion_matrix_yb(model, X, y):
 
 
 # Function to plot classification_report by using yellowbrick
-def plot_classification_report_yb(model, X, y):
-    # Split the dataset into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=12345)
+def plot_classification_report_yb(model):
+    # # Split the dataset into train and test sets
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=123456)
     visualizer = ClassificationReport(model, classes=['not_diabetes', 'has_diabetes'], support=True, cmap='Blues')
     visualizer.fit(X_train, y_train)  # Fit the visualizer and the model
     visualizer.score(X_test, y_test)  # Evaluate the model on the test data
@@ -153,8 +160,8 @@ def plot_classification_report_yb(model, X, y):
 
 # Funtion to plot ROC-AUC Curve
 def plot_roc_auc_curve(model):
-    model_roc_auc = roc_auc_score(y, model.predict(X))
-    fpr, tpr, thresholds = roc_curve(y, model.predict_proba(X)[:, 1])
+    model_roc_auc = roc_auc_score(y_test, model.predict(X_test))
+    fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:, 1])
     plt.figure()
     plt.plot(fpr, tpr, label='AUC (area = %0.2f)' % model_roc_auc)
     plt.plot([0, 1], [0, 1], 'r--')
@@ -168,9 +175,9 @@ def plot_roc_auc_curve(model):
 
 
 # Funtion to plot ROC-AUC Curve by using yellowbrick
-def plot_roc_auc_curve_yb(model, X, y):
-    # Split the dataset into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=12345)
+def plot_roc_auc_curve_yb(model):
+    # # Split the dataset into train and test sets
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=123456)
     visualizer = ROCAUC(model, classes=["not_diabetes", "has_diabetes"])
     visualizer.fit(X_train, y_train)  # Fit the training data to the visualizer
     visualizer.score(X_test, y_test)  # Evaluate the model on the test data
@@ -178,9 +185,9 @@ def plot_roc_auc_curve_yb(model, X, y):
 
 
 # Function to plot prediction errors
-def plot_class_prediction_error_yb(model, X, y):
-    # Split the dataset into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=12345)
+def plot_class_prediction_error_yb(model):
+    # # Split the dataset into train and test sets
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=123456)
     # Instantiate the classification model and visualizer
     visualizer = ClassPredictionError(model, classes=["not_diabetes", "has_diabetes"])
     # Fit the training data to the visualizer
@@ -209,24 +216,20 @@ def report_results_quickly(model):
     visualgrid = [FeatureImportances(model, ax=axes[0][0]),
                   ConfusionMatrix(model, ax=axes[0][1]),
                   ClassificationReport(model, ax=axes[1][0]),
-                  ROCAUC(model, ax=axes[1][1])
-                 ]
-
+                  ROCAUC(model, ax=axes[1][1])]
     for viz in visualgrid:
         viz.fit(X_train, y_train)
         viz.score(X_test, y_test)
         viz.finalize()
-
     plt.show()
 
 
 # Function to plot all the results
-def plot_results(model, X, y):
-    plot_confusion_matrix_yb(model, X, y)
-    plot_classification_report_yb(model, X, y)
-    plot_roc_auc_curve_yb(model, X, y)
-    plot_class_prediction_error_yb(model, X, y)
-
+def plot_results(model):
+    plot_confusion_matrix_yb(model)
+    plot_classification_report_yb(model)
+    plot_roc_auc_curve_yb(model)
+    plot_class_prediction_error_yb(model)
 
 
 # See the results for base models
@@ -246,7 +249,31 @@ base_models = [('LogisticRegression', LogisticRegression()),
                ("CatBoost", CatBoostClassifier(verbose=False)),
                ("NGBoost", NGBClassifier(verbose=False))]
 
-evaluate_classification_model_holdout(base_models, X, y)
+evaluate_classification_model_holdout(base_models)
+
+
+# For some distance-based models, we need to scale the features in order to
+# increase the speed and the performance/accuracy level of the model.
+# We can also see the effect of scaling type by changing the sclaer function.
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
+
+# LogisticRegression with make_pipeline
+logreg = make_pipeline(RobustScaler(), LogisticRegression())
+evaluate_classification_model_holdout([('LogisticRegression', logreg)]) # 0.779 instead of 0.818
+
+
+# KNN with make_pipeline
+knn = make_pipeline(RobustScaler(), KNeighborsClassifier())
+evaluate_classification_model_holdout([('KNN', knn)]) # 0.857 instead of 0.818
+
+# SVC with make_pipeline
+svc = make_pipeline(RobustScaler(), SVC())
+evaluate_classification_model_holdout([('SVM', svc)]) # 0.877 instead of 0.812
+
+# ANN with make_pipeline
+ann = make_pipeline(RobustScaler(), MLPClassifier())
+evaluate_classification_model_holdout([('ANN', ann)]) # 0.870 instead of 0.760
 
 
 # MODEL TUNING
@@ -259,19 +286,19 @@ Models to be tuned:
     - XGBClassifier
 '''
 
-# LogisticRegression # 0.779221
+# LogisticRegression # 0.818
 
 logreg_model = LogisticRegression(random_state=123456)
 logreg_params = {'penalty': ['l1', 'l2'],
                  'C': [0.001, 0.009, 0.01, 0.09, 1, 5, 10, 25]}
 
 logreg_cv_model = GridSearchCV(logreg_model, logreg_params, cv=10, n_jobs=-1, verbose=2).fit(X_train, y_train)
-logreg_cv_model.best_params_ # {'C': 10, 'penalty': 'l2'}
+logreg_cv_model.best_params_ # {'C': 1, 'penalty': 'l2'}
 
 # Final Model
 logreg_tuned = LogisticRegression(**logreg_cv_model.best_params_).fit(X_train, y_train)
 y_pred = logreg_tuned.predict(X_test)
-accuracy_score(y_test, y_pred) # 0.7727272727272727
+accuracy_score(y_test, y_pred) # 0.8181818181818182
 
 # Visualization of Results
 plot_results(logreg_tuned, X_test, y_test)
@@ -279,7 +306,7 @@ report_results_quickly(logreg_tuned)
 plot_learning_curve(logreg_tuned)
 
 
-# RandomForestClassifier # 0.766234
+# RandomForestClassifier # 0.863636
 
 rf_model = RandomForestClassifier(random_state=123456)
 rf_params = {"n_estimators": [100, 200, 500, 1000],
@@ -288,12 +315,12 @@ rf_params = {"n_estimators": [100, 200, 500, 1000],
              "max_depth": [3, 5, 8, None]}
 
 rf_cv_model = GridSearchCV(rf_model, rf_params, cv=10, n_jobs=-1, verbose=2).fit(X_train, y_train)
-rf_cv_model.best_params_ # {'max_depth': 8, 'max_features': 5, 'min_samples_split': 30, 'n_estimators': 100}
+rf_cv_model.best_params_ # {'max_depth': 8, 'max_features': 5, 'min_samples_split': 10, 'n_estimators': 100}
 
 # Final Model
 rf_tuned = RandomForestClassifier(**rf_cv_model.best_params_).fit(X_train, y_train)
 y_pred = rf_tuned.predict(X_test)
-accuracy_score(y_test, y_pred) # 0.7857142857142857
+accuracy_score(y_test, y_pred) # 0.8766233766233766
 
 # Visualization of Results --> Feature Importances
 plot_feature_importances(rf_tuned)
@@ -302,7 +329,7 @@ plot_results(rf_tuned, X, y)
 plot_learning_curve(rf_tuned)
 
 
-# XGBClassifier # 0.779221
+# XGBClassifier # 0.863636
 
 xgb_model = XGBClassifier(random_state=123456)
 xgb_params = {"learning_rate": [0.01, 0.1, 0.2, 1],
@@ -311,12 +338,12 @@ xgb_params = {"learning_rate": [0.01, 0.1, 0.2, 1],
               "n_estimators": [100, 500, 1000]}
 
 xgb_cv_model = GridSearchCV(xgb_model, xgb_params, cv=10, n_jobs=-1, verbose=2).fit(X_train, y_train)
-xgb_cv_model.best_params_ # {'learning_rate': 0.01, 'max_depth': 3, 'min_samples_split': 0.1, 'n_estimators': 100, 'subsample': 0.5}
+xgb_cv_model.best_params_ # {'learning_rate': 0.01, 'max_depth': 8, 'min_samples_split': 0.1, 'n_estimators': 100, 'subsample': 0.9}
 
 # Final Model
 xgb_tuned = XGBClassifier(**xgb_cv_model.best_params_).fit(X_train, y_train)
 y_pred = xgb_tuned.predict(X_test)
-accuracy_score(y_test, y_pred) # 0.7922077922077922
+accuracy_score(y_test, y_pred) # 0.8766233766233766
 
 # Visualization of Results --> Feature Importances
 plot_feature_importances(xgb_tuned)
@@ -325,7 +352,7 @@ plot_results(xgb_tuned, X, y)
 plot_learning_curve(xgb_tuned)
 
 
-# LightGBMClassifier # 0.766234
+# LightGBMClassifier # 0.863636
 
 lgbm_model = LGBMClassifier(random_state=123456)
 lgbm_params = {"learning_rate": [0.01, 0.03, 0.05, 0.1, 0.5],
@@ -338,7 +365,7 @@ lgbm_cv_model.best_params_ # {'learning_rate': 0.01, 'max_depth': 3, 'n_estimato
 # Final Model
 lgbm_tuned = LGBMClassifier(**lgbm_cv_model.best_params_).fit(X_train, y_train)
 y_pred = lgbm_tuned.predict(X_test)
-accuracy_score(y_test, y_pred) # 0.7792207792207793
+accuracy_score(y_test, y_pred) # 0.8701298701298701
 
 
 # Comparison of tuned models
@@ -349,4 +376,42 @@ tuned_models = [('LogisticRegression', logreg_tuned),
                 ('LightGBM', lgbm_tuned)]
 
 
-evaluate_classification_model_holdout(tuned_models, X, y)
+evaluate_classification_model_holdout(tuned_models)
+
+
+# Stack models
+from mlxtend.classifier import StackingCVClassifier
+stack_gen = StackingCVClassifier(classifiers=(logreg_tuned, rf_tuned, xgb_tuned, lgbm_tuned), meta_classifier=xgb_tuned)
+stack_gen_model = stack_gen.fit(X_train, y_train)
+
+evaluate_classification_model_holdout([('stack model', stack_gen_model)]) # 0.890
+
+# Function ot blend models
+def blend_models_predict(X_test):
+    return ((0.1 * logreg_tuned.predict(X_test)) +
+            (0.2 * rf_tuned.predict(X_test)) +
+            (0.2 * xgb_tuned.predict(X_test)) +
+            (0.2 * lgbm_tuned.predict(X_test)) +
+            (0.3 * stack_gen_model.predict(X_test)))
+
+
+accuracy_score(y_test, blend_models_predict(X_test).round()) # 0.8701298701298701
+
+# Pickle Models --> Saving tuned models
+
+# Create a folder named 'Models'
+# save working directory
+
+cur_dir = os.getcwd()
+cur_dir
+
+# change working directory:
+os.chdir('projects\\diabetes_classification\\models')
+
+# Save the models
+for model in tuned_models:
+    pickle.dump(model[1], open(str(model[0]) + ".pkl", 'wb'))
+
+# Load the model that we saved before
+xgb = pickle.load(open(r'C:\Users\yakup\PycharmProjects\dsmlbc\projects\models\XGBoost', 'rb'))
+xgb.predict(X_test)[0:5]
